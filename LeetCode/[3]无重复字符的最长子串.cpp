8151d508fb447f52ac12edcda6ef0498cc6a90ba
bineanzhou <bineanzhou@zhaoxin.com>
2021-11-01 21:41:11 +0800

    add

--- LeetCode/[3]无重复字符的最长子串.cpp
+++ LeetCode/[3]无重复字符的最长子串.cpp
@@ -29,7 +29,14 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-
+		int res = 0, left = 0, i = 0, n = s.size();
+        unordered_map<char, int> m;
+        for (int i = 0; i < n; ++i) {
+            left = max(left, m[s[i]]);
+            m[s[i]] = i + 1;
+            res = max(res, i - left + 1);
+        }
+        return res;
     }
 };
 //leetcode submit region end(Prohibit modification and deletion)
